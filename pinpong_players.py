d370d3b7909996a2623d8d61f5f5e86e40675084
Mate-ber <72556058+Mate-ber@users.noreply.github.com>
2023-10-12 09:48:18 +0700

    Update pinpong_players.py

--- pinpong_players.py
+++ pinpong_players.py
@@ -1,47 +1,20 @@
 import turtle
 
 
-class Player:
+class Player(turtle.Turtle):
     def __init__(self,xxcor):
+        super().__init__()
         self.xxcor = xxcor
-        self.segments = []
-        self.create_player()
-        self.head = self.segments[0]
-        self.head.shape("square")
-        self.head.color("white")
-        self.head.penup()
-        self.player_heading = -1
-
-    def create_player(self):
-        positions = [(self.xxcor, 0), (self.xxcor, 20), (self.xxcor, -20), (self.xxcor, 40), (self.xxcor, -40)]
-        for i in range(5):
-            segment = turtle.Turtle()
-            segment.speed(0)
-            segment.shape("square")
-            segment.color("white")
-            segment.penup()
-            segment.goto(positions[i][0], positions[i][1])
-            self.segments.append(segment)
-        self.head = self.segments[0]
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(self.xxcor, 0)
 
     def change_up(self):
-        for i in range(len(self.segments) - 1, 0, -1):
-            x = self.segments[i - 1].xcor()
-            y = self.segments[i - 1].ycor()
-            self.segments[i].goto(x, y)
-        x = self.head.xcor()
-        y = self.head.ycor()
-        self.head.goto(x, y+20)
+        if self.ycor() + 20 < 250:
+            self.goto(self.xcor(), self.ycor() + 20)
 
     def change_down(self):
-        for i in range(len(self.segments) - 1, 0, -1):
-            x = self.segments[i - 1].xcor()
-            y = self.segments[i - 1].ycor()
-            self.segments[i].goto(x, y - 40)
-        x = self.head.xcor()
-        y = self.head.ycor()
-        self.head.goto(x, y-20)
-
-    def get_cor_player(self):
-        return self.head.xcor(), self.head.ycor()
-
+        if self.ycor() - 20 > -250:
+            self.goto(self.xcor(), self.ycor() - 20)
